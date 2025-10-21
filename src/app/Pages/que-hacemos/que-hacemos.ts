b39cf8b98db2db0e2b699a1c6e690ff3b1f5d50c
Alexander Mascorro <omfattendety@gmail.com>
2025-10-21 01:45:13 -0600

    Eliminar importacion no utilizada de RouterLink

--- src/app/Pages/que-hacemos/que-hacemos.ts
+++ src/app/Pages/que-hacemos/que-hacemos.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-que-hacemos',
-  imports: [RouterLink],
   templateUrl: './que-hacemos.html',
   styleUrl: './que-hacemos.css'
 })
